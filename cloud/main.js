8ba982d19979bc6de9bd374979f3bc1629dcee07
sankalp98 <sankalp.kasle@gmail.com>
2018-01-09 23:40:54 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -15,10 +15,26 @@ Parse.Cloud.define('newincNumOfFollowers', function(request, response) {
   var User = Parse.Object.extend('_User');
   var user = new User();
   var currentUser = new User();
-  currentUser.id = request.params.currentId;
-  user.id = request.params.userId;
+  //currentUser.id = request.params.currentId;
+  //user.id = request.params.userId;
   
-  user.increment("xnumberOfFollowers",+1);
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("objectId", request.params.userId);
+  query.first({
+  useMasterKey: true,
+  success: function(object) {
+  object.increment("xnumberOfFollowers",+1);
+  object.save(null, { useMasterKey: true });
+  // Set the job's success status
+  response.success('Success Message');
+  },
+  error: function(error) {
+  // Set the job's error status
+  response.error('error occured here');
+  }
+});
+  
+  /*user.increment("xnumberOfFollowers",+1);
   user.save(null, {useMasterKey: true},{
   	success :function(user) {
   		response.success('welldone')
@@ -26,7 +42,7 @@ Parse.Cloud.define('newincNumOfFollowers', function(request, response) {
   	error :function(error) {
   		response.error("Could not increment number of followers, error " + error.code + ":" + error.message);
   	}
-  });
+  });*/
 });
 
 Parse.Cloud.define('newdecNumOfFollowers', function(request, response) {
