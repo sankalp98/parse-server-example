e728473145bc585ca3798e8c5ce0c69be8e47b0e
sankalp98 <sankalp.kasle@gmail.com>
2018-01-09 22:59:40 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -10,3 +10,39 @@ Parse.Cloud.define('incNumOfFollowers', function(req, res) {
 Parse.Cloud.define('decNumOfFollowers', function(req, res) {
   res.success('decNumOfFollowers is called');
 });
+
+Parse.Cloud.define('newincNumOfFollowers', function(request, response) {
+  var User = Parse.Object.extend('_User');
+  var user = new User();
+  var currentUser = new User();
+  currentUser.id = request.params.currentId;
+  user.id = request.params.userId;
+  
+  user.increment("xnumberOfFollowers",+1);
+  user.save(null, {useMasterKey: true},{
+  	success :function(user) {
+  		response.success(user)
+  	},
+  	error :function(error) {
+  		response.error("Could not increment number of followers, error " + error.code + ":" + error.message);
+  	}
+  });
+});
+
+Parse.Cloud.define('newdecNumOfFollowers', function(request, response) {
+  var User = Parse.Object.extend('_User');
+  var user = new User();
+  var currentUser = new User();
+  currentUser.id = request.params.currentId;
+  user.id = request.params.userId;
+  
+  user.increment("xnumberOfFollowers",-1);
+  user.save(null, {useMasterKey: true},{
+  	success :function(user) {
+  		response.success(user)
+  	},
+  	error :function(error) {
+  		response.error("Could not decrement number of followers, error " + error.code + ":" + error.message);
+  	}
+  });
+});
