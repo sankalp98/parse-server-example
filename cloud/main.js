10621416c1a07fa0a899b643e3228412a155d080
sankalp98 <sankalp.kasle@gmail.com>
2018-01-10 00:23:20 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -49,16 +49,21 @@ Parse.Cloud.define('newdecNumOfFollowers', function(request, response) {
   var User = Parse.Object.extend('_User');
   var user = new User();
   var currentUser = new User();
-  currentUser.id = request.params.currentId;
-  user.id = request.params.userId;
+  //currentUser.id = request.params.currentId;
+  //user.id = request.params.userId;
   
-  user.increment("xnumberOfFollowers",-1);
-  user.save(null, {useMasterKey: true},{
-  	success :function(user) {
-  		response.success('welldone')
-  	},
-  	error :function(error) {
-  		response.error("Could not decrement number of followers, error " + error.code + ":" + error.message);
-  	}
-  });
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("objectId", request.params.userId);
+  query.first({
+  useMasterKey: true,
+  success: function(object) {
+  object.increment("xnumberOfFollowers",-1);
+  object.save(null, { useMasterKey: true });
+  // Set the job's success status
+  response.success('Success Message');
+  },
+  error: function(error) {
+  // Set the job's error status
+  response.error('error occured here');
+  }
 });
