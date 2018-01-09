a9d8d0fb047dccd5aa30f50be905f5ad7836e734
sankalp98 <sankalp.kasle@gmail.com>
2018-01-09 21:00:10 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,11 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Teri maa ki chuth');
 });
+
+Parse.Cloud.define('incNumOfFollowers', function(req, res) {
+  res.success('incNumOfFollowers is called');
+});
+
+Parse.Cloud.define('decNumOfFollowers', function(req, res) {
+  res.success('decNumOfFollowers is called');
+});
